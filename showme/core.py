eee9740be8854fafef91cd262c1875bdd0b7c963
Kenneth Reitz <me@kennethreitz.com>
2010-09-04 15:10:45 -0400

    Added @trace support. General Updates.

--- showme/core.py
+++ showme/core.py
@@ -1,32 +1,23 @@
-from showme.packages.decorator import decorator
-
 #         ______                                      
 # ___________  /_ ______ ___      _________ ___ _____ 
 # __  ___/__  __ \_  __ \__ | /| / /__  __ `__ \_  _ \
 # _(__  ) _  / / // /_/ /__ |/ |/ / _  / / / / //  __/
 # /____/  /_/ /_/ \____/ ____/|__/  /_/ /_/ /_/ \___/ 
 
-__version__ = '0.0.2'
+from showme.packages.decorator import decorator
+
+import cProfile
+import inspect
+import pprint
+
+
+__version__ = '0.0.3'
 __author__ = 'Kenneth Reitz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2010 Kenneth Reitz'
 
 
-import cProfile
-import pprint
-
-
 pp = pprint.PrettyPrinter(indent=4)
-
-
-@decorator
-def args(f, *args, **kwargs):
-	"""Display passed arguments of given function"""
-	
-	# print('Arguments: \n  %s\n	%s' % (args, kwargs))
-	# pprint.PrettyPrinter(kwargs)
-	pp.pprint(kwargs)
-	return f(*args, **kwargs)
 
 
 @decorator
@@ -52,19 +43,32 @@ def globals(f, *args, **kwargs):
 @decorator
 def locals(f, *args, **kwargs):
 	"""Display local variables"""
-	
+	# print locals()
 	return f(*args, **kwargs)
 
 
 @decorator
 def docs(f, *args, **kwargs):
 	"""Display PyDocs of given function"""
-	
+	print inspect.getdoc(f)
 	return f(*args, **kwargs)
 
 
 @decorator
-def size(f, *args, **kwargs):
-	return f(*args, **kwargs)
-	
+def trace(f, *args, **kwargs):
+    """Print scope, call, and argument information."""
+
+    _scope = inspect.getmodule(f).__name__
+
+    # guess that function is a method of it's class
+    if f.func_name in dir(args[0].__class__):
+        _scope +=  '.' + args[0].__class__.__name__
+    else:
+        _scope +=  '.' + f.__name__
+
+    print("calling %s() with \nargs: %s \nkwargs: %s" % (_scope, args, kwargs))
+
+    return f(*args, **kwargs)
+
+
 
