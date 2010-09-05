4ce9d274c0570379dcb8aa7e7e2c2ef923e66bde
Kenneth Reitz <me@kennethreitz.com>
2010-09-05 16:04:32 -0400

    Pip depenency fix.

--- setup.py
+++ setup.py
@@ -3,33 +3,47 @@
 
 import os
 import sys
-import showme
 
 from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+	publish()
+	sys.exit()
 
 def publish():
 	"""Publish to PyPi"""
 	os.system("python setup.py sdist upload")
 
-if sys.argv[-1] == "publish":
-	publish()
-	sys.exit()
-
-setup(name='showme',
-	  version=showme.core.__version__,
-	  description='Painless Debugging and Inspection for Python',
-	  long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
-	  author='Kenneth Reitz',
-	  author_email='me@kennethreitz.com',
-	  url='http://github.com/kennethreitz/showme',
-	  packages=['showme'],
-	  license='MIT',
-	  classifiers = ( 
-		"Development Status :: 4 - Beta",
-		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python",
-		"Programming Language :: Python :: 2.5",
-		"Programming Language :: Python :: 2.6",
-		"Programming Language :: Python :: 2.7",
-		)
-	 )
+setup(
+	name='showme',
+	version='1.0.0',
+	description='Painless Debugging and Inspection for Python',
+	long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+	author='Kenneth Reitz',
+	author_email='me@kennethreitz.com',
+	url='http://github.com/kennethreitz/showme',
+	packages=['showme'],
+	install_requires=[
+		'colorama>=0.1.18', 
+		'decorator>=3.2.0'
+	],
+	license='MIT',
+	classifiers = ( 
+	  'Development Status :: 5 - Production/Stable',
+	  'Environment :: Console',
+	  'Intended Audience :: Developers',
+	  'License :: OSI Approved :: MIT License',
+	  'Programming Language :: Python',
+	  'Programming Language :: Python :: 2.5',
+	  'Programming Language :: Python :: 2.6',
+	  'Programming Language :: Python :: 2.7',
+	  'Operating System :: MacOS :: MacOS X',
+	  'Operating System :: Unix',
+	  'Operating System :: POSIX',
+	  'Topic :: Software Development',
+	  'Topic :: Software Development :: Libraries',
+	  'Topic :: Software Development :: Libraries :: Python Modules',
+	  )
+)
+	
+    
