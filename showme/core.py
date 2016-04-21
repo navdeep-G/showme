f060d46b0f6a52e5c5db2169590e4fe37555ce4c
rajat404 <404rajat@gmail.com>
2016-04-21 21:04:47 +0530

    added python3 compatibility

--- showme/core.py
+++ showme/core.py
@@ -27,67 +27,67 @@ init(autoreset=True)
 
 @decorator
 def cputime(f, *args, **kwargs):
-	"""Display CPU Time statistics of given function."""
+    """Display CPU Time statistics of given function."""
 
-	print('CPU time for %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET))
+    print(('CPU time for %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET)))
 
-	t = cProfile.Profile()
-	r = t.runcall(f, *args, **kwargs)
-	t.print_stats()
+    t = cProfile.Profile()
+    r = t.runcall(f, *args, **kwargs)
+    t.print_stats()
 
-	return r
+    return r
 
-	
+
 @decorator
 def docs(f, *args, **kwargs):
-	"""Display Docstrings of given function."""
+    """Display Docstrings of given function."""
 
-	print('Documentation for %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET))
-	print(inspect.getdoc(f))
-	
-	return f(*args, **kwargs)
+    print(('Documentation for %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET)))
+    print((inspect.getdoc(f)))
+
+    return f(*args, **kwargs)
 
 
 @decorator
 def time(f, *args, **kwargs):
-	"""Display Runtime statistics of given function."""
+    """Display Runtime statistics of given function."""
 
-	print('Execution speed of %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET))
-	_t0 = now()
-	_r = f(*args, **kwargs)
-	_t1 = now()
+    print(('Execution speed of %s%s%s:' % (Fore.CYAN, _get_scope(f, args), Fore.RESET)))
+    _t0 = now()
+    _r = f(*args, **kwargs)
+    _t1 = now()
 
-	total_time =  _t1-_t0
-	print('%s seconds' % (total_time))
+    total_time = _t1 - _t0
+    print(('%s seconds' % (total_time)))
 
-	return _r
+    return _r
 
 
 @decorator
 def trace(f, *args, **kwargs):
-	"""Display epic argument and context call information of given function."""
+    """Display epic argument and context call information of given function."""
 
-	_scope = _get_scope(f, args)
+    _scope = _get_scope(f, args)
 
-	print("Calling %s%s%s with: \n   %sargs%s: %s \n   %skwargs%s: %s" % (
-		Fore.CYAN, _scope, Fore.RESET, Fore.BLUE, Fore.RESET,
-		args, Fore.BLUE, Fore.RESET, kwargs))
+    print(("Calling %s%s%s with: \n   %sargs%s: %s \n   %skwargs%s: %s" % (
+        Fore.CYAN, _scope, Fore.RESET, Fore.BLUE, Fore.RESET,
+        args, Fore.BLUE, Fore.RESET, kwargs)))
 
-	return f(*args, **kwargs)
+    return f(*args, **kwargs)
 
 
 def _get_scope(f, args):
-	"""Get scope nameo of given function."""
+    """Get scope nameo of given function."""
 
-	_scope = inspect.getmodule(f).__name__
-	# guess that function is a method of it's class
-	try:
-		if f.func_name in dir(args[0].__class__):
-			_scope += '.' + args[0].__class__.__name__
-			_scope += '.' + f.__name__
-		else:
-			_scope += '.' + f.__name__
-	except IndexError:
-		_scope += '.' + f.__name__
+    _scope = inspect.getmodule(f).__name__
+    # guess that function is a method of it's class
+    try:
+        if f.__name__ in dir(args[0].__class__):
+            _scope += '.' + args[0].__class__.__name__
+            _scope += '.' + f.__name__
+        else:
+            _scope += '.' + f.__name__
+    except IndexError:
+        _scope += '.' + f.__name__
 
-	return _scope
+    return _scope
